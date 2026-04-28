870ce5960fe7da2f1a6455344e846481a8857d8c
Sinchana <sinchana.aiml@gmail.com>
2026-04-28 21:14:56 +0530

    fixed proxy connection to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:10000'
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
     }
   }
 })
